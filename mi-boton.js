774cf1f8c98b6e7d9b75fcdc269fe2167172d8ac
eduarame <eduarame@suramericana.com.co>
2024-02-02 20:29:25 -0500

    Se agrega template con slot e interactividad con custom events

--- mi-boton.js
+++ mi-boton.js
@@ -1,25 +1,55 @@
 
 // Definimos la clase para nuestro componente
 class MiBoton extends HTMLElement {
+  static get observedAttributes() {
+    return ["titulo"];
+  }
+
   constructor() {
     super(); // Llamada al constructor de la clase padre (HTMLElement)
     // Se adjunta el shadow DOM al custom element
+
     this.attachShadow({ mode: "open" });
-    // Se añade el HTML interno y los estilos al shadow DOM
-    this.shadowRoot.innerHTML = `
-        <style>
-          button {
-            background-color: #4CAF50;
-            color: white;
-            padding: 14px 20px;
-            margin: 10px;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-          }
-        </style>
-        <button><slot>Click me</slot></button>
-      `;
+  }
+
+  showButton(rootElement) {
+    const template = document.getElementById("boton-template").content;
+    const titulo = this.getAttribute("titulo");
+    const alertaMessage = this.getAttribute("alerta-message");
+    const h1Element = template.querySelector("h1#__TITULO__");
+    const buttonElement = template.querySelector("button#__BUTTON__");
+
+    if (rootElement) {
+      if (h1Element) h1Element.innerText = titulo;
+
+      if (buttonElement) {
+        buttonElement.addEventListener("click", function (e) {
+          e.preventDefault();
+
+          this.dispatchEvent(
+            new CustomEvent("seleccionar", {
+              detail: {
+                mensaje: alertaMessage,
+              },
+              bubbles: true,
+              composed: true,
+              cancelable: false,
+            })
+          );
+        });
+      }
+
+      // Se añade el HTML interno y los estilos al shadow DOM
+      rootElement.appendChild(template);
+    }
+  }
+
+  connectedCallback() {
+    if (this.shadowRoot) this.showButton(this.shadowRoot);
+  }
+
+  attributeChangedCallback() {
+    if (this.shadowRoot) this.showButton(this.shadowRoot);
   }
 }
 
